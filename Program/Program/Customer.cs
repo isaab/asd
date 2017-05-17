1d70b62a8fa8cffe2afb2559afdcb680c49e1ba7
P301 <P301>
2017-05-17 21:17:53 +0400

    first commit

---
+++ Program/Program/Customer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Operators;
+
+namespace Costumers
+{
+    class Costumer
+    {
+        string name;
+        int costumer_start_x, costumer_start_y, costumer_end_x, costumer_end_y;
+
+        public Costumer(string name, int start_x, int start_y, int end_x, int end_y)
+        {
+            this.name = name;
+            this.costumer_start_x = start_x;
+            this.costumer_start_y = start_y;
+            this.costumer_end_x = end_x;
+            this.costumer_end_y = end_y;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+
+        public int Costumer_end_x
+        {
+            get
+            {
+                return this.costumer_end_x;
+            }
+        }
+
+        public int Costumer_end_y
+        {
+            get
+            {
+                return this.costumer_end_y;
+            }
+        }
+
+        public string costumerInputs()
+        {
+            return this.name + "-" + this.costumer_start_x + "-" + this.costumer_start_y;
+        }
+
+        public double distanceCalculator(int end_x, int end_y)
+        {
+            double distance = Math.Sqrt(Math.Pow(end_x - this.costumer_start_x, 2) + Math.Pow(end_y - this.costumer_start_y, 2));
+            return Math.Round(distance);
+        }
+
+        public int taxiSelection(List<Taxi> taxiList, List<double> distanceList, Costumer cos)
+        {
+            int position = 0;
+            double minDistance;
+            for (int i = 0; i < taxiList.Count; i++)
+            {
+                distanceList.Add(cos.distanceCalculator(taxiList[i].Taxi_start_x, taxiList[i].Taxi_start_y));
+            }
+
+            minDistance = distanceList[0];
+            for (int i = 0; i < distanceList.Count; i++)
+            {
+                if (distanceList[i] < minDistance)
+                {
+                    position = i;
+
+                }
+            }
+
+            distanceList.Clear();
+            return position;
+        }
+    }
+}
---
+++ Program/Program/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Costumers;
+using Operators;
+
+namespace taxi_service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Costumer senem = new Costumer("Senem", 40, 50, 100, 150);
+           // Costumer nigar = new Costumer("Nigar", 15, 15, 40, 100);
+           // Costumer ferid = new Costumer("Ferid", 25, 70, 5, 25);
+
+            List<Taxi> taxiList = new List<Taxi>();
+            List<double> distanceList = new List<double>();
+
+            taxiList.Add(new Taxi("10-HY-123", 50, 60));
+            taxiList.Add(new Taxi("10-XD-562", 10, 35));
+            taxiList.Add(new Taxi("10-UR-821", 25, 55));
+            taxiList.Add(new Taxi("10-XS-821", 15, 20));
+
+            int positionS = senem.taxiSelection(taxiList, distanceList, senem);
+            //int positionN = nigar.taxiSelection(taxiList, distanceList, nigar);
+           // int positionF = ferid.taxiSelection(taxiList, distanceList, ferid);
+
+            Console.WriteLine("\n" + "Costumer inputs: " + senem.costumerInputs());
+            Operator operatorS = new Operator(senem, taxiList[positionS]);
+
+           // Console.WriteLine("\n" + "Costumer inputs: " + nigar.costumerInputs());
+            //Operator operatorN = new Operator(nigar, taxiList[positionN]);
+
+           //Console.WriteLine("\n" + "Costumer inputs: " + ferid.costumerInputs());
+            //Operator operatorF = new Operator(ferid, taxiList[positionF]);
+
+            Console.ReadKey();
+        }
+    }
+}
